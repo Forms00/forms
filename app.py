6fb0a94abf3fb84c2cfd6f000e0845a0e067b55a
anon <anon@mail.com>
2026-06-20 05:09:43 -0500

    initial

--- app.py
+++ app.py
@@ -262,5 +262,9 @@ def generate_pdf(data):
                      mimetype="application/pdf")
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
